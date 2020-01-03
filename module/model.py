5aad8a9565993570b69dc9a5f08b7f7c07d92a20
itsmystyle <itsmystyle.0821@gmail.com>
2020-01-03 22:46:39 +0800

    added multiple backbone

--- module/model.py
+++ module/model.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from pretrainedmodels.models import (
+    se_resnext50_32x4d,
+    se_resnext101_32x4d,
+    se_resnet152,
+)
 
 from module.layers.utils import Flatten
 from module.layers.metric_learning_utils import GeM, ArcMarginProduct
@@ -66,6 +71,84 @@ class SeResNet50(nn.Module):
     def extract_features(self, x):
         x = self.backbone(x)
         x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        x = self.bn(x)
+
+        return x
+
+
+class SeResNet152(nn.Module):
+    def __init__(self, num_classes, fc_dim):
+        super(SeResNet152, self).__init__()
+
+        self.num_classes = num_classes
+        self.fc_dim = fc_dim
+
+        self.backbone = se_resnet152(num_classes=1000, pretrained="imagenet")
+        final_in_features = self.backbone.last_linear.in_features
+
+        self.fc = nn.Linear(final_in_features * 10 * 10, self.fc_dim)
+        self.bn = nn.BatchNorm1d(self.fc_dim)
+        self._init_params()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.fc_dim, self.num_classes), nn.LogSoftmax(dim=1)
+        )
+
+    def _init_params(self):
+        nn.init.xavier_normal_(self.fc.weight)
+        nn.init.constant_(self.fc.bias, 0)
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
+
+    def forward(self, x):
+        x = self.extract_features(x)
+        x = self.classifier(x)
+
+        return x
+
+    def extract_features(self, x):
+        x = self.backbone.features(x)
+        x = self.backbone.avg_pool(x).view(x.shape[0], -1)
+        x = self.fc(x)
+        x = self.bn(x)
+
+        return x
+
+
+class SeResNeXt50(nn.Module):
+    def __init__(self, num_classes, fc_dim):
+        super(SeResNeXt50, self).__init__()
+
+        self.num_classes = num_classes
+        self.fc_dim = fc_dim
+
+        self.backbone = se_resnext50_32x4d(num_classes=1000, pretrained="imagenet")
+        final_in_features = self.backbone.last_linear.in_features
+
+        self.fc = nn.Linear(final_in_features * 10 * 10, self.fc_dim)
+        self.bn = nn.BatchNorm1d(self.fc_dim)
+        self._init_params()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.fc_dim, self.num_classes), nn.LogSoftmax(dim=1)
+        )
+
+    def _init_params(self):
+        nn.init.xavier_normal_(self.fc.weight)
+        nn.init.constant_(self.fc.bias, 0)
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
+
+    def forward(self, x):
+        x = self.extract_features(x)
+        x = self.classifier(x)
+
+        return x
+
+    def extract_features(self, x):
+        x = self.backbone.features(x)
+        x = self.backbone.avg_pool(x).view(x.shape[0], -1)
         x = self.fc(x)
         x = self.bn(x)
 
@@ -171,6 +254,62 @@ class SeResNetArcFaceModel(nn.Module):
     def extract_features(self, x):
         batch_size = x.shape[0]
         x = self.backbone(x)
+        x = self.pooling(x).view(batch_size, -1)
+
+        if self.use_fc:
+            x = self.fc(x)
+            x = self.bn(x)
+
+        return x
+
+
+class SeResNeXtArcFaceModel(nn.Module):
+    def __init__(
+        self, n_classes, scale, margin, fc_dim, model=50, use_fc=False, device=None,
+    ):
+        super(SeResNeXtArcFaceModel, self).__init__()
+        if model == 50:
+            self.backbone = se_resnext50_32x4d(num_classes=1000, pretrained="imagenet")
+        else:
+            self.backbone = se_resnext101_32x4d(num_classes=1000, pretrained="imagenet")
+        final_in_features = self.backbone.last_linear.in_features
+
+        self.pooling = GeM()
+        final_in_features = 2048
+
+        self.use_fc = use_fc
+        if use_fc:
+            self.fc = nn.Linear(2048, fc_dim)
+            self.bn = nn.BatchNorm1d(fc_dim)
+            self._init_params()
+            final_in_features = fc_dim
+
+        self.final = ArcMarginProduct(
+            final_in_features,
+            n_classes,
+            multi_task=False,
+            s=scale,
+            m=margin,
+            device=device,
+        )
+
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def _init_params(self):
+        nn.init.xavier_normal_(self.fc.weight)
+        nn.init.constant_(self.fc.bias, 0)
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
+
+    def forward(self, x, label):
+        x = self.extract_features(x)
+        logits = self.final(x, label)
+
+        return self.logsoftmax(logits)
+
+    def extract_features(self, x):
+        batch_size = x.shape[0]
+        x = self.backbone.features(x)
         x = self.pooling(x).view(batch_size, -1)
 
         if self.use_fc:
