ab590b0d041801e4489f52acdef6ab91ede27f46
itsmystyle <itsmystyle.0821@gmail.com>
2020-01-04 16:49:29 +0800

    added triplet and center loss

--- module/loss.py
+++ module/loss.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from module.utils import normalize, euclidean_dist, hard_example_mining
+
 
 class CrossEntropyLabelSmooth(nn.Module):
     """Cross entropy loss with label smoothing regularizer.
@@ -20,7 +22,91 @@ class CrossEntropyLabelSmooth(nn.Module):
 
     def forward(self, inputs, targets):
         device = targets.device
-        targets = torch.zeros(inputs.shape).scatter_(1, targets.unsqueeze(1).cpu(), 1).to(device)
+        targets = (
+            torch.zeros(inputs.shape)
+            .scatter_(1, targets.unsqueeze(1).cpu(), 1)
+            .to(device)
+        )
         targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
         loss = (-targets * inputs).mean(0).sum()
         return loss
+
+
+class CenterLoss(nn.Module):
+    """Center loss.
+    Reference:
+    Wen et al. A Discriminative Feature Learning Approach for Deep Face Recognition. ECCV 2016.
+    Args:
+        num_classes (int): number of classes.
+        feat_dim (int): feature dimension.
+    """
+
+    def __init__(self, num_classes=751, feat_dim=2048, device="cpu"):
+        super(CenterLoss, self).__init__()
+        self.num_classes = num_classes
+        self.feat_dim = feat_dim
+        self.device = device
+
+        self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim)).to(
+            device
+        )
+
+    def forward(self, x, labels):
+        """
+        Args:
+            x: feature matrix with shape (batch_size, feat_dim).
+            labels: ground truth labels with shape (num_classes).
+        """
+        assert x.size(0) == labels.size(
+            0
+        ), "features.size(0) is not equal to labels.size(0)"
+
+        batch_size = x.size(0)
+        distmat = (
+            torch.pow(x, 2)
+            .sum(dim=1, keepdim=True)
+            .expand(batch_size, self.num_classes)
+            + torch.pow(self.centers, 2)
+            .sum(dim=1, keepdim=True)
+            .expand(self.num_classes, batch_size)
+            .t()
+        )
+        distmat.addmm_(1, -2, x, self.centers.t())
+
+        classes = torch.arange(self.num_classes).long().to(self.device)
+        labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
+        mask = labels.eq(classes.expand(batch_size, self.num_classes))
+
+        dist = []
+        for i in range(batch_size):
+            value = distmat[i][mask[i]]
+            value = value.clamp(min=1e-12, max=1e12)  # for numerical stability
+            dist.append(value)
+        dist = torch.cat(dist)
+        loss = dist.mean()
+        return loss
+
+
+class TripletLoss(object):
+    """Modified from Tong Xiao's open-reid (https://github.com/Cysu/open-reid).
+    Related Triplet Loss theory can be found in paper 'In Defense of the Triplet
+    Loss for Person Re-Identification'."""
+
+    def __init__(self, margin=None):
+        self.margin = margin
+        if margin is not None:
+            self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+        else:
+            self.ranking_loss = nn.SoftMarginLoss()
+
+    def __call__(self, global_feat, labels, normalize_feature=False):
+        if normalize_feature:
+            global_feat = normalize(global_feat, axis=-1)
+        dist_mat = euclidean_dist(global_feat, global_feat)
+        dist_ap, dist_an = hard_example_mining(dist_mat, labels)
+        y = dist_an.new().resize_as_(dist_an).fill_(1)
+        if self.margin is not None:
+            loss = self.ranking_loss(dist_an, dist_ap, y)
+        else:
+            loss = self.ranking_loss(dist_an - dist_ap, y)
+        return loss, dist_ap, dist_an
